be754bb2cd339318266c236c76d5b7a8304ff31a
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-20 18:26:04 +0100

    fix pep8

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,16 +4,8 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    
     id = int(argv[1])
-    
-    # Make requests to the API
-    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={id}")
 
-    # Print status codes
-    print(f"TODOs API Status Code: {todos_response.status_code}")
-
-    
     url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
     todos_response = requests.get(url)
     todos_data = todos_response.json()
@@ -23,9 +15,7 @@ if __name__ == "__main__":
     for todo in todos_data:
         if todo["completed"] is True:
             done += 1
-    print(
-        "Employee {} is done with tasks({}/{}):".format(name, done, len(todos_data))
-    )
+    print("Employee {} is done with tasks({}/{}):".format(name, done, len(todos_data)))
     for todo in todos_data:
         if todo["completed"]:
             print("\t {}".format(todo["title"]))
