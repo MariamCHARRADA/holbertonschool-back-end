3fa308c4b395b7fb59cef40a09b9e0ba7a6b05cb
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-20 18:24:10 +0100

    finally gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -4,21 +4,28 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
+    
     id = int(argv[1])
-    url = "https://jsonplaceholder.typicode.com/"
-    request = requests.get(f"{url}users/{id}")
-    data = request.json()
-    name = data.get("name")
-    todos = requests.get(f"{url}todos?userId={id}").json()
+    
+    # Make requests to the API
+    todos_response = requests.get(f"https://jsonplaceholder.typicode.com/todos?userId={id}")
+
+    # Print status codes
+    print(f"TODOs API Status Code: {todos_response.status_code}")
+
+    
+    url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
+    todos_response = requests.get(url)
+    todos_data = todos_response.json()
+    name = todos_data[0].get("username")
 
     done = 0
-    for todo in todos:
+    for todo in todos_data:
         if todo["completed"] is True:
             done += 1
     print(
-        "Employee {} is done with tasks({}/{}):".format(name,
-                                                        done,
-                                                        len(todos)))
-    for todo in todos:
+        "Employee {} is done with tasks({}/{}):".format(name, done, len(todos_data))
+    )
+    for todo in todos_data:
         if todo["completed"]:
             print("\t {}".format(todo["title"]))
