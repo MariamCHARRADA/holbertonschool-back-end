083d78d04cfc1023ea572f5f85f585f19f578231
MariamCHARRADA <charradamariam@yahoo.com>
2023-12-20 18:38:20 +0100

    fix username :D

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,11 +6,16 @@ from sys import argv
 if __name__ == "__main__":
     id = int(argv[1])
 
-    url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
-    todos_response = requests.get(url)
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
+    todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
-    name = todos_data[0].get("username")
-
+    
+    user_url = f"https://jsonplaceholder.typicode.com/users/{id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+    name = user_data["name"]
+    
+    
     done = 0
     for todo in todos_data:
         if todo["completed"] is True:
