46ad077f151d6b658a449428ab69d20adeeee514
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-06 22:41:32 +0100

    Dictionary

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -3,11 +3,8 @@
 import json
 import requests
 
-
-
 if __name__ == "__main__":
-
-
+    
     todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={id}"
     todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
@@ -19,7 +16,14 @@ if __name__ == "__main__":
     username = user_data["username"]
 
     filename = "todo_all_employees.json"
-    todo_list = []
+    try:
+        with open(filename, "r") as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        data = {}
+
+    todo_list = data.get(id, [])
+
     for todo in todos_data:
         task_info = {
             "task": todo["title"],
@@ -27,5 +31,8 @@ if __name__ == "__main__":
             "username": username,
         }
         todo_list.append(task_info)
+
+    data[id] = todo_list
+
     with open(filename, "w") as f:
-        json.dump({id: todo_list}, f)
+        json.dump(data, f)
